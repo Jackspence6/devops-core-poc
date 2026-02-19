3f808934e77e911b7f896b6ab9bffefe104ffbc2
Jack Spence <jackspence.dev@gmail.com>
2026-02-19 16:56:44 +0200

    Adds reusable VPC module with subnet support

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,4 +1,31 @@
-variable "vpc_id" {
-  description = "The VPC ID to launch ECS tasks in"
+variable "cidr_block" {
+  description = "VPC CIDR block"
   type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets_cidrs" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "azs" {
+  description = "Availability zones to use"
+  type        = list(string)
+  default     = ["us-east-1", "us-east-2"]
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default = {
+    Project = "devops-core-poc"
+  }
 }
