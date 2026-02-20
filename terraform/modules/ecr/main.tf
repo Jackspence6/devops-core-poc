01546f2833a3fd3883d2ec0c3b189455ddda636c
Jack Spence <jackspence.dev@gmail.com>
2026-02-20 15:29:25 +0200

    Updates ecr

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,19 +1,27 @@
+# ECR Repository
 resource "aws_ecr_repository" "this" {
-  # Creates a repo in AWS ECR to store our Docker images for deployment to ECS
+  # Creates a repo in AWS ECR to store our Docker images for ECS
   name                 = var.ecr_repository_name
   image_tag_mutability = "MUTABLE"
 }
 
+# Lifecycle Policy
 resource "aws_ecr_lifecycle_policy" "this" {
-  # Automatically cleans up old untagged images to save storage and avoid clutter
   repository = aws_ecr_repository.this.name
+
   policy = jsonencode({
     rules = [
       {
         rulePriority = 1
-        description  = "Expire old images"
-        selection    = { tagStatus = "untagged", countType = "imageCountMoreThan", countNumber = 10 }
-        action       = { type = "expire" }
+        description  = "Expire untagged images beyond 10"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+        action = {
+          type = "expire"
+        }
       }
     ]
   })
--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -1,11 +1,9 @@
-# The full URL of the repo (used for tagging & pushing Docker images)
 output "repository_url" {
-  value       = aws_ecr_repository.finance_tracker_repository.repository_url
+  value       = aws_ecr_repository.this.repository_url
   description = "The URL of the ECR repository for Docker image pushes and pulls"
 }
 
-# The Amazon Resource Name (ARN) of the ECR repository
 output "repository_arn" {
-  value       = aws_ecr_repository.finance_tracker_repository.arn
+  value       = aws_ecr_repository.this.arn
   description = "The ARN of the ECR repository"
 }
